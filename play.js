4cdc4eb12aa7b9582d1bd5959404ca7ff2d74a9a
arpipatel-ap <arpi96@gmail.com>
2024-05-08 23:41:05 -0400

    Add function setupInput

--- play.js
+++ play.js
@@ -1,17 +1,5 @@
 const connect = require("./client")
 const handleUserInput = function (data) {
-  if (key === 'w') {
-    conn.write("Move: up");
-  }
-  if (key === 'a') {
-    conn.write("Move: left");
-  }
-  if (key === 's') {
-    conn.write("Move: down");
-  }
-  if (key === 'd') {
-    conn.write("Move: right");
-  }
   if (data === '\u0003') {
     process.exit();
   }
